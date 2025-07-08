144e418e3348c25c4d6074a3d685ccdd6b7eea04
manishkumarguptadev <manishkumarguptadev@gmail.com>
2025-07-08 13:36:00 +0530

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+
 function App() {
-  return <div className="flex h-screen items-center justify-center">App</div>;
+  const pathname = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+  return (
+    <>
+      <main className="mx-auto min-h-screen max-w-7xl">
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
 export default App;
